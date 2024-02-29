7dff2908fefc3005e780d182f5e5c5e43ef4fbbe
Trohat <petr.sed@centrum.cz>
2024-02-29 21:19:11 +0100

    Model Zvire

--- informace/models.py
+++ informace/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Zvire(models.Model):
+    jmeno = models.CharField(max_length=30)
+    druh = models.CharField(max_length=30)
+    vek = models.IntegerField()
+    popis = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.druh} {self.jmeno} má {self.vek} let."
+    
+# LAZY EVALUATION
+
---
+++ povidani/crud.py
@@ -0,0 +1,26 @@
+"""
+
+#CREATE:
+opice = Zvire(jmeno="Jitka", druh="gorila", vek=13, popis="Má ráda banány")
+opice.save()
+
+nebo zkratka:
+Zvire.objects.create(druh="Člověk", jmeno="Adam", popis="Je to alfasamec.", vek=30)
+
+#READ:
+Zvire.objects.all()
+Zvire.objects.get(jmeno="Alík") 
+Zvire.objects.get(id=3) 
+Zvire.objects.get(pk=4) 
+Zvire.objects.filter(jmeno="Alík").filter(vek__gte=10).filter(popis__contains="hlídá")
+
+
+#UPDATE:
+opice.jmeno = "Táňa"
+opice.save()
+
+#DELETE:
+opice.delete()
+
+
+"""
