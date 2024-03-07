8f6b715a31fc48c0210598ee757af5f3882b6095
Trohat <petr.sed@centrum.cz>
2024-03-07 19:30:00 +0100

    View funkce o_zvireti

--- informace/urls.py
+++ informace/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("zirafa/", views.zirafa, name="zirafa"),
+    # path("zirafa/", views.zirafa, name="zirafa"),
     path("seznam/", views.seznam, name="seznam_zvirat"),
-    path("<int:zvire>/", views.cislo),
-    path("<str:zvire>/", views.o_zvireti, name="zvire"),
-    path("<kategorie>/<druh>/", views.taxonomie),
+    # path("<int:zvire>/", views.cislo),
+    path("<int:id>/", views.o_zvireti, name="zvire"),
+    # path("<kategorie>/<druh>/", views.taxonomie),
     path("", views.o_zviratech, name="o_zviratech")
 ]
 
--- informace/views.py
+++ informace/views.py
@@ -6,34 +6,17 @@ from .models import Zvire
 
 # Create your views here.
 
-slovnik = {
-    "opice": "Opice má palce podobné jako člověk.",
-    "pes": "Pes je přítel člověka.",
-    "kocka": "Šelma a zároveň mazlíček.",
-    "kralik": "Králík ma dlouhé uši.",
-    "tucnak": "Rád skáče do vody a plave.",
-    "slon": "Slon troubí hodně nahlas."
-}
-
-diakritika = {
-    "kocka": "kočka",
-    "kralik": "králík",
-    "tucnak": "tučňák"
-}
-
-def o_zvireti(request, zvire):
-    print(f"Přišel mi parametr {zvire}")
-    if zvire in slovnik:
-        popis = slovnik[zvire]
-        if zvire in diakritika:
-            zvire = diakritika[zvire]
-     
+def o_zvireti(request, id):
+    try:
+        zvire = Zvire.objects.get(pk=id)
+        popis = zvire.popis.rstrip(".")
         return render(request, "informace/zvire.html", {
-            "popis": popis,
-            "jmeno_zvirete": zvire
+            "zvire": zvire,
+            "nadpis": f"{zvire.druh.lower()} {zvire.jmeno.title()}",
+            "popis": popis
         })
-    else:
-        return HttpResponseNotFound(f"{zvire} není v naší zoo.")
+    except:
+        return HttpResponseNotFound(f"Zvíře s číslem {id} není v naší zoo.")
 
 def seznam(request):
     zvirata = Zvire.objects.all()
@@ -41,20 +24,7 @@ def seznam(request):
         "seznam": zvirata
     })
 
-def zirafa(request):
-    # load template
-    # create template    
-    # load context  
-    # return template with context
-    # create HttpResponse
-    min_pocet_pro_akci = 10
-    seznam = ["a", "b"]
-    chybi_do_deseti = min_pocet_pro_akci - len(seznam)
-    return render(request, "informace/zirafa.html", {
-        "delka_krku": 3,
-        "seznam_ziraf": seznam,
-        "chybi": chybi_do_deseti
-    })
+
 
 def o_zviratech(request):
     return render(request, "informace/o_zviratech.html", {
@@ -73,8 +43,26 @@ def index(request):
         "pocet": pocet_zvirat
     })
 
+"""
+starý kód, na něm jsme se učili routování a filtry v šablonách
+def zirafa(request):
+    # load template
+    # create template    
+    # load context  
+    # return template with context
+    # create HttpResponse
+    min_pocet_pro_akci = 10
+    seznam = ["a", "b"]
+    chybi_do_deseti = min_pocet_pro_akci - len(seznam)
+    return render(request, "informace/zirafa.html", {
+        "delka_krku": 3,
+        "seznam_ziraf": seznam,
+        "chybi": chybi_do_deseti
+    })
+
 def cislo(request, zvire):
     return HttpResponse(f"<h2>Číslo zvířete je {zvire}</h2>")
 
 def taxonomie(request, kategorie, druh):
     return HttpResponse(f"<p>Kategorie je {kategorie}.</p><p>Druh je {druh}.</p>")
+"""
