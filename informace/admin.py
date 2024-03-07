e5406a06d266ec7660c320ae0d46fc55af267d72
Trohat <petr.sed@centrum.cz>
2024-03-07 21:08:04 +0100

    Vazba many-to-one

--- informace/admin.py
+++ informace/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 
-from .models import Zvire
+from .models import Zvire, Pavilon
 
 # Register your models here.
 
 class ZvireAdmin(admin.ModelAdmin):
-    list_display = ("id", "jmeno", "druh", "popis", "vek", "barva")
+    list_display = ("id", "jmeno", "druh", "popis", "vek", "barva", "pavilon")
     list_filter = ("jmeno", "druh", "vek")
 
+class PavilonAdmin(admin.ModelAdmin):
+    list_display = ("id", "jmeno", "umisteni")
 
 admin.site.register(Zvire, ZvireAdmin)
+
+admin.site.register(Pavilon, PavilonAdmin)
 
 
 
--- informace/models.py
+++ informace/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Pavilon(models.Model):
+    jmeno = models.CharField(max_length=30)
+    umisteni = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.jmeno
+
+
 class Zvire(models.Model):
     jmeno = models.CharField(max_length=30)
     druh = models.CharField(max_length=30)
     vek = models.IntegerField()
     popis = models.CharField(max_length=200)
     barva = models.CharField(max_length=30, null=True)
-
+    pavilon = models.ForeignKey(Pavilon, on_delete = models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.druh} {self.jmeno} má {self.vek} let."
@@ -17,5 +25,5 @@ class Zvire(models.Model):
         verbose_name_plural = "Zvirata"
         ordering = ("jmeno", "-id")
     
-# LAZY EVALUATION
+
 
